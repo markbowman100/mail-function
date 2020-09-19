fac539691df3bb314086443bb6e9f6b0926641d0
unknown <markbowman100@gmail.com>
2020-09-19 11:35:26 -0400

    Updates to use mailchimp

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 "use strict";
-const nodemailer = require("nodemailer");
+const mailgun = require("mailgun-js");
 
 exports.sendEmail = (req, res) => {
 
@@ -12,44 +12,46 @@ exports.sendEmail = (req, res) => {
     res.set('Access-Control-Max-Age', '3600');
     res.status(204).send('');
   } else {
-    let name = req.body.name;
-    let email = req.body.email;
-    let subject = req.body.subject;
-    let message = req.body.message;
-    if(name !== undefined && name !== null && 
-      email !== undefined && email !== null && 
-      subject !== undefined && subject !== null && 
-      message !== undefined && message !== null) {
+    if (process.env.APP_APIKEY == req.headers["x-api-key"]) {
+      let name = req.body.name;
+      let email = req.body.email;
+      let subject = req.body.subject;
+      let message = req.body.message;
+      if(name !== undefined && name !== null && 
+        email !== undefined && email !== null && 
+        subject !== undefined && subject !== null && 
+        message !== undefined && message !== null) {
 
-      sendEmail(name, email, subject, message).then(() => {
-        res.status(200).send();
-      }).catch((err) => {
-        res.status(500).send(err);
-      })
+        sendEmail(name, email, subject, message).then(() => {
+          res.status(200).send();
+        }).catch((err) => {
+          res.status(500).send(err);
+        })
+      }
+      else {
+        res.status(400).send("Missing mandatory fields");
+      }
     }
     else {
-      res.status(400).send("Missing mandatory fields");
+      res.status(403).send();
     }
   }
 };
 
 async function sendEmail(name, email, subject, message) {
-  let credentials = {
-    service: 'Gmail',
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  };
-  let transporter = nodemailer.createTransport(credentials);
   // send mail with defined transport object
-  const emailTemplate = {
+  const DOMAIN = process.env.DOMAIN;
+  const APIKEY = process.env.APIKEY;
+  const data = {
     from: `${email}`,
     to: process.env.EMAIL_USERNAME,
     subject: subject,
     text: `${name} ${email}:${message}`,
     html: `<p>${name} ${email}:${message}</p>`
   };
-  let info = await transporter.sendMail(emailTemplate);
-  return info;
+  const mg = mailgun({apiKey: APIKEY, domain: DOMAIN});
+
+  mg.messages().send(data, function (error, body) {
+    return error;
+  });
 }
